ff72babbdac837ee023e6b5d6b6b4b11ebd3cb00
sai <sai_prasad_veluri@yahoo.com>
2025-08-08 06:22:06 +0000

    Group by

--- LINQDemoReVisit/Form1.Designer.cs
+++ LINQDemoReVisit/Form1.Designer.cs
@@ -29,23 +29,65 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Location = new Point(84, 86);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(110, 86);
             button1.TabIndex = 0;
-            button1.Text = "Where";
+            button1.Text = "Where and ORder by";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(304, 35);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Group by";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(240, 86);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(254, 264);
+            listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(582, 86);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(254, 264);
+            listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(1003, 86);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(254, 264);
+            listBox3.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1361, 524);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -55,5 +97,9 @@
         #endregion
 
         private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
     }
 }
--- LINQDemoReVisit/Form1.cs
+++ LINQDemoReVisit/Form1.cs
@@ -12,10 +12,13 @@ namespace LINQDemoReVisit
         List<Employee> emplist = new List<Employee>() {
         new Employee{ Empid=1,EmpName="Aditi",Deptno=10,Salary=17777},
         new Employee{ Empid=2,EmpName="Amit",Deptno=20,Salary=23323223},
-        new Employee{ Empid=3,EmpName="Sumit",Deptno=23,Salary=10000},
+        new Employee{ Empid=3,EmpName="Sumit",Deptno=22,Salary=10000},
         new Employee{ Empid=4,EmpName="Rajesh",Deptno=10,Salary=18888},
         new Employee{ Empid=5,EmpName="Ramesh",Deptno=10,Salary=277777},
-        new Employee{ Empid=6,EmpName="Suresh",Deptno=30,Salary=34444
+        new Employee{ Empid=6,EmpName="Suresh",Deptno=30,Salary=34444 },
+            new Employee{ Empid=7,EmpName="Sowmya",Deptno=30,Salary=34444 },
+                new Employee{ Empid=8,EmpName="Suramya",Deptno=30,Salary=34444,
+
         }
 
         };
@@ -28,9 +31,9 @@ namespace LINQDemoReVisit
 
             var findEmployee = from emp in emplist
                                orderby emp.Empid
-                                where emp.Empid == 6 || emp.Empid == 2
-                                select emp;
-              foreach (var item in findEmployee)
+                               where emp.Empid == 6 || emp.Empid == 2
+                               select emp;
+            foreach (var item in findEmployee)
             {
                 MessageBox.Show(item.Empid.ToString());
                 MessageBox.Show(item.EmpName);
@@ -42,7 +45,7 @@ namespace LINQDemoReVisit
             MessageBox.Show("METHOD SYNTAX....");
             var foundEmployee = emplist.Where(emp => emp.Empid == 6 || emp.Empid == 2).OrderBy(empdata => empdata.Empid);
 
-            foreach (var item in foundEmployee) 
+            foreach (var item in foundEmployee)
             {
                 MessageBox.Show(item.Empid.ToString());
                 MessageBox.Show(item.EmpName);
@@ -51,7 +54,76 @@ namespace LINQDemoReVisit
                 MessageBox.Show("One record completed....");
 
             }
-                           
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var result = emplist.GroupBy(emp => emp.Deptno).
+                Select(emp=> new { groupedDeptno = emp.Key,
+                    EmpCount=emp.Count(),
+                    EmployeesInDept=emp});
+
+            foreach (var item in result)
+            {
+                listBox1.Items.Add("Deptno= " + item.groupedDeptno +
+                    " EmpCount= " + item.EmpCount);
+                foreach (var item1 in item.EmployeesInDept)
+                {
+                    listBox1.Items.Add(item1.Empid);
+                    listBox1.Items.Add(item1.EmpName);
+                    listBox1.Items.Add(item1.Deptno);
+                    listBox1.Items.Add(item1.Salary);
+                    listBox1.Items.Add("-------");
+
+                }
+            }
+
+
+            var result2 = emplist.GroupBy(emp => emp.Deptno).
+               Select(emp => new {  emp.Key,empcount=Convert.ToInt32(emp.Count()),  emp });
+            foreach (var item in result2)
+            {
+                listBox2.Items.Add("Deptno= " + item.Key + " EmpCount= " + item.empcount);
+                foreach (var item1 in item.emp)
+                {
+                    listBox2.Items.Add(item1.Empid);
+                    listBox2.Items.Add(item1.EmpName);
+                    listBox2.Items.Add(item1.Deptno);
+                    listBox2.Items.Add(item1.Salary);
+                    listBox2.Items.Add("-------");
+
+                }
+            }
+
+
+
+
+
+
+            var result1 = from emp in emplist
+                          group emp by emp.Deptno into groupedon
+                          select new {groupedDeptno= groupedon.Key, EmpCount=groupedon.Count(), 
+                              EmployeesInDept=groupedon   };
+
+            foreach (var item in result1) {
+                listBox3.Items.Add("Deptno= " + item.groupedDeptno + " EmpCount= " + item.EmpCount);
+                foreach (var item1 in item.EmployeesInDept)
+                {
+                    listBox3.Items.Add(item1.Empid);
+                    listBox3.Items.Add(item1.EmpName);
+                    listBox3.Items.Add(item1.Deptno);
+                    listBox3.Items.Add(item1.Salary);
+                    listBox3.Items.Add("-------");
+                }
+
+
+
+
+            }
+
+
 
         }
     }
